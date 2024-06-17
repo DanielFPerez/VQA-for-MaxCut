0d8a72d4bd7aac74fb48488688a4b23a51800fb3
Daniel Felipe Perez Ramirez <daniel.felipe.perez.ramirez@ri.se>
2024-06-17 23:07:37 +0200

    scripts created

---
+++ utils.py
@@ -0,0 +1,27 @@
+import pickle
+import networkx as nx
+from typing import List
+
+
+def load_pickle(src_dir: str) -> object:
+    with open(src_dir, 'rb') as f:
+        return pickle.load(f)   
+    
+
+def save_pickle(data: object, dest_dir: str) -> int:
+    with open(dest_dir, 'wb') as f:
+        pickle.dump(data, f)
+    print(f"Data saved to {dest_dir}.")    
+    return 0
+
+
+# Generate random graphs
+def generate_random_graphs(num_graphs: int, num_nodes: int, edge_prob: float = 0.5) -> List[nx.Graph]:
+    graphs = [nx.erdos_renyi_graph(num_nodes, edge_prob) for _ in range(num_graphs)]
+    # set random positions for nodes
+    for elem in graphs:
+        nx.set_node_attributes(elem, nx.spring_layout(elem), 'pos')
+    return graphs
+
+def show_graph(graph: nx.Graph, font: str = 'white', node_size: int = 600):
+    return nx.draw(graph,pos=graph.nodes(data='pos'), with_labels=True, font_color=font, node_size=node_size)
